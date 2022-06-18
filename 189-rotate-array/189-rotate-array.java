e6582717d3da743ccf1caae1856c6ced74ab073c
harivanshb <79466803+harivanshb@users.noreply.github.com>
2022-06-18 13:07:08 -0300

    Time: 7 ms (5.36%), Space: 63.9 MB (74.29%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public void rotate(int[] nums, int k) {
+        k=k%nums.length;
+        if(nums.length>1){
+            
+        swap(nums, 0, nums.length - 1);
+        swap(nums, 0, k - 1);
+        swap(nums, k, nums.length - 1);
+        }
+    }
+
+    public void swap(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp;
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

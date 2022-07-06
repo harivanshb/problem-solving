569ea374230ef0dc449a99f878128344a555c9ef
harivanshb <79466803+harivanshb@users.noreply.github.com>
2022-07-05 21:20:46 -0300

    Time: 17 ms (11.11%), Space: 40.1 MB (82.34%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,11 @@
 class Solution {
     public int fib(int n) {
-      return calculateFib(n);
-    }
-    public static int calculateFib(int n){
-        if(n==1){
+       if(n==1){
             return 1;
         }
         if(n==0){
             return 0;
         }
-        else return calculateFib(n-1) + calculateFib(n-2);
+        else return fib(n-1) + fib(n-2);
     }
 }

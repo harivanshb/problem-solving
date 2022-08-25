fcb60440bc2d47b1e93739d768bac6908efb171a
harivanshb <79466803+harivanshb@users.noreply.github.com>
2022-08-24 22:34:33 -0300

    Time: 5 ms (74.47%), Space: 44.9 MB (85.10%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] rn = new int[26];
-        boolean isNotZero = false;
+        
         for(int i=0;i<ransomNote.length();i++){
-            if(rn[ransomNote.charAt(i)-'a']==0)
-            {
-                rn[ransomNote.charAt(i)-'a']=1;
-            }else{
                 rn[ransomNote.charAt(i)-'a']++;
             }
-        }
+        
             for(int i=0;i<magazine.length();i++){
                 if(rn[magazine.charAt(i)-'a']!=0){
                     rn[magazine.charAt(i)-'a']--;
@@ -17,11 +13,9 @@ class Solution {
             }
             for(int i=0;i<rn.length;i++){
                 if(rn[i]!=0){
-                    isNotZero = true;
+                    return false;
                 }
             }
-            if(isNotZero){
-                return false;
-            }return true;
+        return true;
 }
 }
